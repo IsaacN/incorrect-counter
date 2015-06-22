1e88d791f4aa7c4998f48b333e253dbeaa12dc80
Isaac Nankavill <me@isaacnankavill.com>
2015-06-22 21:43:23 +1000

    Base Style and Markup

--- index.php
+++ index.php
@@ -4,24 +4,50 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Incorrectness Counter</title>
-
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <link href='css/style.css' rel='stylesheet'>
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
-    <div class='jumbotron' style='background-color: #333'>
+    <div class='jumbotron leader'>
+      <div class='wrapper leader'>
+        <h2 class='name leader'>Jordan</h2>
+        <p class='count leader'>123</p>
+      </div>
     </div>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <div class='runnersUp'>
+      <div class='container-fluid'>
+        <div class='col-md-3'>
+          <div class='wrapper'>
+            <h3 class='name second'>Andy</h3>
+            <p class='count'>100</p>
+          </div>
+        </div>
+        <div class='col-md-3'>
+          <div class='wrapper'>
+            <h3 class='name third'>Isaac</h3>
+            <p class='count'>50</p>
+          </div>
+        </div>
+        <div class='col-md-3'>
+          <div class='wrapper'>
+            <h3 class='name fourth'>Alex</h3>
+            <p class='count'>25</p>
+          </div>
+        </div>
+        <div class='col-md-3'>
+          <div class='wrapper'>
+            <h3 class='name fifth'>Nick</h3>
+            <p class='count'>10</p>
+          </div>
+        </div>
+      </div><!--end container-fluid-->
+    </div><!--end .runnersUp-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
