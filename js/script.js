ff473de6b0854b51807e59ed00ae204d6f42e44a
Isaac Nankavill <me@isaacnankavill.com>
2015-06-23 15:21:28 +1000

    User position
    Set up JS to establish user position when their add button is clicked

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	$('.add').click(function(){
+		var positionWhole = ($(this).attr('class').split(' ')[1]);
+		var pattern = /[0-9]+/g;
+		var matches = positionWhole.match(pattern);
+		//gets position number of user
+	});
 	$('.name').mouseenter(function(){
 		var className = $(this).attr('class').split(' ')[1];
 		$('.add.'+className).fadeIn(100);
@@ -7,4 +13,5 @@ $(document).ready(function(){
 		var className = $(this).find('.add').attr('class').split(' ')[1];
 		$('.add.'+className).fadeOut(100);
 	});
+	
 });
