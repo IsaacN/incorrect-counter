499f3f2a3590bbba0b0de7388a4a48f1b92798c2
Isaac Nankavill <me@isaacnankavill.com>
2015-06-24 16:37:21 +1000

    Made NULL comment submission

--- popupsubmit.php
+++ popupsubmit.php
@@ -5,10 +5,16 @@ include('dbconn.php');
   $comment = mysql_real_escape_string($_POST['comment']);
   $reporter = mysql_real_escape_string($_POST['reporter']);
   $commenter_id_full = explode('user_id_',$_POST['commenter_id']);
-  $commenter_id = mysql_real_escape_string($commenter_id_full[1]);
+  $commenter_id = mysql_real_escape_string($commenter_id_full[1]);{}
 
-  $sql = "INSERT INTO comments (commentID, commenterID, reporterID, comment, timestamp)
+  if($comment == ''){
+  	$sql = "INSERT INTO comments (commentID, commenterID, reporterID, comment, timestamp)
+        VALUES ('','".$commenter_id."','".$reporter."', NULL,'".date('y-m-d H:i:s')."')";
+  }
+  else{
+  	$sql = "INSERT INTO comments (commentID, commenterID, reporterID, comment, timestamp)
         VALUES ('','".$commenter_id."','".$reporter."','".$comment."','".date('y-m-d H:i:s')."')";
+  }
 
   $query = mysql_query($sql);
 ?>
